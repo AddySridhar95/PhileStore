6f3f9131f250eda8aa7553578d231b99c3306737
Aditya Sridhar <aditya123ss@gmail.com>
2016-08-22 00:29:46 -0700

    trying to read philes

--- app/src/main/java/com/example/store/phile/philestore/MainActivity.java
+++ app/src/main/java/com/example/store/phile/philestore/MainActivity.java
@@ -1,52 +1,46 @@
 package com.example.store.phile.philestore;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.LinearLayout;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        Log.d("MainActivity", Environment.getExternalStorageState());
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        File path = new File(Environment.getExternalStorageDirectory().toString());
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Log.d("main_activity", path.getAbsolutePath());
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Log.d("MainActivity", path.exists() ? "File exists" : "File doesn't exist");
+
+        try {
+            Boolean result = path.mkdirs();
+            Log.d("MainActivity", result ? "Success" : "Failure");
+        } catch (SecurityException e) {
+            Log.d("MainActivity", "SECURITY EXCEPTION");
         }
 
-        return super.onOptionsItemSelected(item);
+        Log.d("MainActivity", path.exists() ? "File exists" : "File doesn't exist");
+
+        File[] sdFiles = path.listFiles();
+
+        if (sdFiles == null) {
+            Log.d("MainActivity", "SD files is null");
+        } else {
+            for(int i = 0; i < sdFiles.length; i++) {
+                Log.d("MainActivity", sdFiles[i].getAbsolutePath());
+            }
+        }
+
     }
 }
