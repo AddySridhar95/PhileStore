f898b25b413ed7173f0a24a355c0b28d9783b0ab
Aditya Sridhar <aditya123ss@gmail.com>
2016-09-05 18:04:05 +0530

    fixed delete bug and finished implementing copy

--- app/src/main/java/com/example/store/phile/philestore/MainActivity.java
+++ app/src/main/java/com/example/store/phile/philestore/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.store.phile.philestore;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -13,7 +12,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.InputType;
 import android.util.Log;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -23,7 +21,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 
@@ -32,42 +35,20 @@ public class MainActivity extends AppCompatActivity implements ListFileFragment.
     private String path = Environment.getExternalStorageDirectory().toString();
     private ArrayList<FileListItem> fileListItems = new ArrayList<>();
     private ArrayList<FileListItem> clipboard = new ArrayList<>();
+    private String clipboardOperation = "move";
+
     private Toolbar myToolbar;
+    private FilenameFilter filter = new FilenameFilter() {
+        @Override
+        public boolean accept(File dir, String filename) {
+            File sel = new File(dir, filename);
 
-
-
-    private void prepareFileItemsFromPath() {
-
-        // TODO: for sorting, sort fileListItems according to sort order here
-
-        FilenameFilter filter = new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String filename) {
-                File sel = new File(dir, filename);
-
-                // Filters based on whether the file is hidden or not
-                return (sel.isFile() || sel.isDirectory()) && !sel.isHidden();
-            }
-        };
-
-        fileListItems.clear();
-        String[] files = (new File(path)).list(filter);
-        for(int i = 0; i < files.length; i++) {
-
-            // parentPath is the path of the directory the file in question is in.
-            String parentPath = path;
-            if (!parentPath.endsWith(File.separator)) {
-                parentPath = parentPath + File.separator;
-            }
-
-            FileListItem fileListItem = new FileListItem(files[i], parentPath);
-            fileListItems.add(fileListItem);
+            // Filters based on whether the file is hidden or not
+            return (sel.isFile() || sel.isDirectory()) && !sel.isHidden();
         }
-    }
+    };
 
-    public ArrayList<FileListItem> getFileListItems() {
-        return fileListItems;
-    }
+    // --------- Handlers --------------
 
     @Override
     public void onFileItemClicked(String p) {
@@ -91,18 +72,6 @@ public class MainActivity extends AppCompatActivity implements ListFileFragment.
         return true;
     }
 
-    private boolean renameFile(File from, File to) {
-        return from.exists() && from.getParentFile().exists() && !to.exists() && from.renameTo(to);
-    }
-
-    private boolean deleteFile(File file) {
-        return file.exists() && file.delete();
-    }
-
-//    private boolean moveFile() {
-//
-//    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -120,7 +89,6 @@ public class MainActivity extends AppCompatActivity implements ListFileFragment.
                     public void onClick(DialogInterface dialog, int which) {
                         FileListItem toBeRenamed = getFileItemsSelected().get(0);
                         File fileToBeRenamed = new File(toBeRenamed.getFilePath() + toBeRenamed.getFileName());
-                        Log.d("MainAct", toBeRenamed.getFilePath() + input.getText().toString());
                         File fileToBeRenamedTo = new File(toBeRenamed.getFilePath() + input.getText().toString());
                         boolean renameStatus = renameFile(fileToBeRenamed, fileToBeRenamedTo);
 
@@ -168,7 +136,6 @@ public class MainActivity extends AppCompatActivity implements ListFileFragment.
 
                 return true;
 
-            // TODO test this
             case R.id.action_refresh:
                 prepareFileItemsFromPath();
                 restartListFragment();
@@ -180,71 +147,77 @@ public class MainActivity extends AppCompatActivity implements ListFileFragment.
 
             // For move and copy use createNewFile/mkdir maybe? Can move multiple files, folders and a mixture of the two
             case R.id.action_move:
-                ArrayList<FileListItem> selectedItems = new ArrayList<>();
-                clipboard = selectedItems;
-                for (int i = 0; i < selectedItems.size(); i++) {
-                    selectedItems.get(i).setIsSelected(false);
+                ArrayList<FileListItem> selectedMoveItems = getFileItemsSelected();
+
+                // save selected file items in the clipboard
+                clipboard = selectedMoveItems;
+                clipboardOperation = "move";
+
+                // mark each selected item as unselected
+                for (int i = 0; i < selectedMoveItems.size(); i++) {
+                    selectedMoveItems.get(i).setIsSelected(false);
                 }
 
                 restartListFragment();
-                // mark all selected items as unselected
-                // put them in clipboard
+
+
+                // TODO Error message when trying to move root/abc to root/abc/child_abc/
+                // TODO fire dialog to inform user about clipboard and paste operation.
                 return true;
 
             case R.id.action_copy:
+                ArrayList<FileListItem> selectedCopyItems = getFileItemsSelected();
+
+                // save selected file items in the clipboard
+                clipboard = selectedCopyItems;
+                clipboardOperation = "copy";
+
+                // mark each selected item as unselected
+                for (int i = 0; i < selectedCopyItems.size(); i++) {
+                    selectedCopyItems.get(i).setIsSelected(false);
+                }
+
+                restartListFragment();
                 return true;
 
             case R.id.action_paste:
+                for (int i = 0; i < clipboard.size(); i++) {
+
+                    String parentPath = path;
+                    if (!parentPath.endsWith(File.separator)) {
+                        parentPath = parentPath + File.separator;
+                    }
+
+                    File clipboardFile = new File(clipboard.get(i).getFilePath() + clipboard.get(i).getFileName());
+                    File targetFile = new File(parentPath + clipboard.get(i).getFileName());
+                    if (clipboardOperation == "move") {
+
+                        boolean moveStatus = renameFile(clipboardFile, targetFile);
+                        if (moveStatus) {
+                            prepareFileItemsFromPath();
+                            restartListFragment();
+                        } else {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Move operation failed", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    } else {
+                        boolean copyStatus = copyFilesOrDirectories(clipboardFile, targetFile);
+                        if (copyStatus) {
+                            prepareFileItemsFromPath();
+                            restartListFragment();
+                        } else {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Copy operation failed", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    }
+
+                }
                 return true;
 
             default:
                 return super.onOptionsItemSelected(item);
 
         }
-    }
-
-    private void setToolbarStyles() {
-        int noFileItemsSelected = noFileItemsSelected();
-
-        // TODO: based on no of items selected hide menu items appropriately
-
-        if (noFileItemsSelected > 0) {
-            myToolbar.setTitle(noFileItemsSelected + " selected");
-            myToolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
-            myToolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorToolbarSelected));
-        } else {
-            myToolbar.setNavigationIcon(null);
-
-            // TODO: What should be the title when no item is selected
-            myToolbar.setTitle("PhileStore");
-            myToolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorToolbarDefault));
-        }
-    }
-
-    private void restartListFragment() {
-        setToolbarStyles();
-
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ListFileFragment fragment = (ListFileFragment) fm.findFragmentByTag("pho_tag");
-
-        ft.detach(fragment).attach(fragment);
-        ft.commit();
-    }
-
-    private ArrayList<FileListItem> getFileItemsSelected() {
-        ArrayList<FileListItem> fileListItemsSelected = new ArrayList<>();
-        for (int i = 0; i < fileListItems.size(); i++) {
-            if (fileListItems.get(i).getIsSelected()) {
-                fileListItemsSelected.add(fileListItems.get(i));
-            }
-        }
-
-        return fileListItemsSelected;
-    }
-
-    private int noFileItemsSelected() {
-        return getFileItemsSelected().size();
     }
 
     @Override
@@ -286,58 +259,6 @@ public class MainActivity extends AppCompatActivity implements ListFileFragment.
             });
         }
 
-    }
-
-    public void dialogInputBox() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Create folder");
-        final EditText input = new EditText(this);
-        builder.setView(input);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String creationError = "";
-
-                if (input.getText() != null && !input.getText().toString().isEmpty()) {
-                    String parentPath = path;
-                    if (!parentPath.endsWith(File.separator)) {
-                        parentPath = parentPath + File.separator;
-                    }
-
-                    String folderName = input.getText().toString();
-                    File target = new File(parentPath + folderName);
-
-                    if (!target.exists()) {
-                        boolean status = target.mkdir();
-
-                        if (!status) {
-                            creationError = "Folder creation failed";
-                        }
-
-                    } else {
-                        // TODO: item already exists, do you want to replace dialog
-                        creationError = "Folder already exists";
-                    }
-                } else {
-                    creationError = "Folder name not valid";
-                }
-
-                if (creationError.isEmpty()) {
-                    Toast toast = Toast.makeText(getApplicationContext(), creationError, Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-
-                prepareFileItemsFromPath();
-                restartListFragment();
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        builder.show();
     }
 
     @Override
@@ -390,5 +311,203 @@ public class MainActivity extends AppCompatActivity implements ListFileFragment.
 
             onFileItemClicked(newPath);
         }
+    }
+
+    // --------- Helpers --------------
+
+    private void prepareFileItemsFromPath() {
+        // TODO: for sorting, sort fileListItems according to sort order here
+        fileListItems.clear();
+        String[] files = (new File(path)).list(filter);
+        for(int i = 0; i < files.length; i++) {
+
+            // parentPath is the path of the directory the file in question is in.
+            String parentPath = path;
+            if (!parentPath.endsWith(File.separator)) {
+                parentPath = parentPath + File.separator;
+            }
+
+            FileListItem fileListItem = new FileListItem(files[i], parentPath);
+            fileListItems.add(fileListItem);
+        }
+    }
+
+    private boolean renameFile(File from, File to) {
+        return from.exists() && !to.exists() && from.renameTo(to);
+    }
+
+    // TODO: fix delete by looping over all children and deleting one by one
+    private boolean deleteFile(File file) {
+
+        if (!file.exists()) {
+            return false;
+        }
+
+        if (file.isDirectory()) {
+            String[] contents = file.list();
+            for (int i = 0; i < contents.length; i++) {
+
+                // TODO error handling
+                boolean a = deleteFile(new File(file.getAbsolutePath(), contents[i]));
+            }
+        }
+        return file.delete();
+    }
+
+    public void copy(File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dst);
+
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+    }
+
+    private boolean copyFilesOrDirectories(File from, File to) {
+        if (!from.exists()) {
+            return false;
+        }
+
+        if (from.isDirectory()) {
+            if (!to.exists()) {
+                to.mkdir();
+            }
+
+            String[] files = from.list();
+            // Log.d("copyFilesOrDirectories", files.length + "");
+            for(int i = 0; i < files.length; i++) {
+
+                // parentPath is the path of the directory the file in question is in.
+                String parentPath = from.getAbsolutePath();
+                if (!parentPath.endsWith(File.separator)) {
+                    parentPath = parentPath + File.separator;
+                }
+
+                Log.d("copyFilesOrDirectories", parentPath + files[i]);
+                Log.d("copyFilesOrDirectories", new File(to.getAbsolutePath(), files[i]).getAbsolutePath());
+                // TODO error handling
+                boolean a = copyFilesOrDirectories(new File(parentPath + files[i]), new File(to.getAbsolutePath(), files[i]));
+            }
+
+            Log.d("copyFilesOrDirectories", "directory!!!!");
+            return true;
+        } else {
+            Log.d("copyFilesOrDirectories", "file!!!!");
+            try {
+                copy(from, to);
+                return true;
+            } catch (IOException ex) {
+
+            }
+            return false;
+        }
+    }
+
+    private void setToolbarStyles() {
+        int noFileItemsSelected = noFileItemsSelected();
+
+        // TODO: based on no of items selected hide menu items appropriately
+
+        if (noFileItemsSelected > 0) {
+            myToolbar.setTitle(noFileItemsSelected + " selected");
+            myToolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+            myToolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorToolbarSelected));
+        } else {
+            myToolbar.setNavigationIcon(null);
+
+            // TODO: What should be the title when no item is selected
+            myToolbar.setTitle("PhileStore");
+            myToolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorToolbarDefault));
+        }
+    }
+
+    private void restartListFragment() {
+        setToolbarStyles();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ListFileFragment fragment = (ListFileFragment) fm.findFragmentByTag("pho_tag");
+
+        ft.detach(fragment).attach(fragment);
+        ft.commit();
+    }
+
+    private ArrayList<FileListItem> getFileItemsSelected() {
+        ArrayList<FileListItem> fileListItemsSelected = new ArrayList<>();
+        for (int i = 0; i < fileListItems.size(); i++) {
+            if (fileListItems.get(i).getIsSelected()) {
+                fileListItemsSelected.add(fileListItems.get(i));
+            }
+        }
+
+        return fileListItemsSelected;
+    }
+
+    private int noFileItemsSelected() {
+        return getFileItemsSelected().size();
+    }
+
+    private void dialogInputBox() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Create folder");
+        final EditText input = new EditText(this);
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String creationError = "";
+
+                if (input.getText() != null && !input.getText().toString().isEmpty()) {
+                    String parentPath = path;
+                    if (!parentPath.endsWith(File.separator)) {
+                        parentPath = parentPath + File.separator;
+                    }
+
+                    String folderName = input.getText().toString();
+                    File target = new File(parentPath + folderName);
+
+                    if (!target.exists()) {
+                        boolean status = target.mkdir();
+
+                        if (!status) {
+                            creationError = "Folder creation failed";
+                        }
+
+                    } else {
+                        // TODO: item already exists, do you want to replace dialog
+                        creationError = "Folder already exists";
+                    }
+                } else {
+                    creationError = "Folder name not valid";
+                }
+
+                if (creationError != "") {
+                    Toast toast = Toast.makeText(getApplicationContext(), creationError, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+                prepareFileItemsFromPath();
+                restartListFragment();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+
+
+    // --------- Getters & setters ------------------
+
+    public ArrayList<FileListItem> getFileListItems() {
+        return fileListItems;
     }
 }
